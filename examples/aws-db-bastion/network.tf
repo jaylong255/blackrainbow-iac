7d28cf5db2082f1aeb2a2150255702c5e4e19873
Jay <jaylong255@gmail.com>
2023-12-06 19:40:46 +0000

    adding db and public subnets

--- examples/aws-db-bastion/network.tf
+++ examples/aws-db-bastion/network.tf
@@ -11,9 +11,26 @@ module "vpc" {
   cidr = local.vpc_cidr
   azs             = local.azs
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 4)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 8)]
+
+  create_database_subnet_group = true
+  single_nat_gateway           = true
+  one_nat_gateway_per_az       = false
+  external_nat_ip_ids          = [aws_eip.nat_eip.id]
+  enable_dns_hostnames         = true
+  enable_nat_gateway           = true
 
   tags = {
     Name = "${local.project_name}-${local.environment}-vpc"
+    Environment = local.environment
+    Project = local.project_name
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "${local.project_name}-${local.environment}-nat-eip"
     Environment = local.environment
     Project = local.project_name
   }
