877e3857130702c222473b9aeaf3d57c5940b8dd
Jay Long <jaylong255@gmail.com>
2023-12-07 16:36:41 -0600

    adding project vars

--- examples/redshift-to-rds/variables.tf
+++ examples/redshift-to-rds/variables.tf
@@ -7,3 +7,18 @@ variable "aws_secret_key" {
   type        = string
   description = "The AWS secret key to use for the AWS provider"
 }
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to use for the AWS provider"
+}
+
+variable "project_name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment to deploy to"
+}
