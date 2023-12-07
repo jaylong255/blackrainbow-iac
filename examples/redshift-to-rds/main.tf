fec2523c215cd071120f87f9fc41773ad560069e
Jay Long <jaylong255@gmail.com>
2023-12-07 10:46:14 -0600

    can't use variables here

--- examples/redshift-to-rds/main.tf
+++ examples/redshift-to-rds/main.tf
@@ -1,17 +1,23 @@
 # backend in aws s3, aws provider that authenticates with key and secret
+
 terraform {
   backend "s3" {
-    bucket = var.state_bucket
-    key    = "examples/redshift-to-rds/terraform.tfstate"
+    bucket = "cyber-terraform-state-us-east-1"
+    key    = "blackrainbow-iac/examples/redshift-to-rds/terraform.tfstate"
     region = "us-east-1"
   }
-  required_version = ">= 1.6.5"
+  required_version = ">= 1.4.5"
   required_providers {
     aws = {
-      source     = "hashicorp/aws"
-      version    = "~> 4.30.0"
-      access_key = var.aws_access_key
-      secret_key = var.aws_secret_key
+      source  = "hashicorp/aws"
+      version = "~> 4.30.0"
     }
   }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
 }
--- examples/redshift-to-rds/variables.tf
+++ examples/redshift-to-rds/variables.tf
@@ -1,8 +1,3 @@
-variable "state_bucket" {
-  type        = string
-  description = "The name of the S3 bucket to store the Terraform state file in"
-}
-
 variable "aws_access_key" {
   type        = string
   description = "The AWS access key to use for the AWS provider"
